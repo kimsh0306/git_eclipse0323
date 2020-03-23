705027f340e7adf6fa6d252ee55909b932e15c27
kimsh0306 <b666790@gmail.com>
2020-03-23 15:43:45 +0900

    hotfix브랜치로 파일 추가함. 03:43

---
+++ dev_book/src/hotfix/HotfixTest.java
@@ -0,0 +1,10 @@
+package hotfix;
+
+public class HotfixTest {
+
+	public static void main(String[] args) {
+	//프로젝트명 => 오른쪽 클릭 => Remote => Switch => New Branch => hotfix(이름)
+		System.out.println("hotfix 브랜치에서 추가한 소스임.");
+	}
+
+}
